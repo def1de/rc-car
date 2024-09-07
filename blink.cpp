754010e8387b1a18dfe504321983b7cd85f0ba32
Illia Katerynych <iliakat.kr@gmail.com>
2024-09-07 11:31:14 +0100

    Refactored for atmega328p

--- blink.cpp
+++ blink.cpp
@@ -2,19 +2,14 @@
 #include <avr/io.h>
 #include <util/delay.h>
 
-int main(void) {
-    // Set PD5 (pin for RX LED) as output
-    DDRD |= (1 << PD5);  // PD5 is now an output pin
+int main(void){
+    DDRB = DDRB | (1 << DDB5);
 
-    while (1) {
-        // Turn the RX LED on (active low)
-        PORTD &= ~(1 << PD5);  // Clear bit PD5 to 0 (LOW)
-        _delay_ms(1000);       // Wait for 1 second
-
-        // Turn the RX LED off
-        PORTD |= (1 << PD5);   // Set bit PD5 to 1 (HIGH)
-        _delay_ms(1000);       // Wait for 1 second
+    while(1){
+        PORTB = PORTB | (1 << PORTB5);
+        _delay_ms(100);
+        PORTB = PORTB & ~(1 << PORTB5);
+        _delay_ms(100);
     }
-
-    return 0;  // Should never reach this
 }
+
